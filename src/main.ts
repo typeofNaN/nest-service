1fee77fa7575e31767fd0ed6a4a5738e9334c292
typeofNaN <dmdefine6@gmail.com>
2020-11-02 17:35:41 +0800

    perfect

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@app/app.module';
+import { isDevMode } from '@app/app.environment';
+import { APP_CONFIG } from '@app/app.config'
+
+const { log, warn, info } = console;
+const color = c => isDevMode ? c : '';
+Object.assign(global.console, {
+  log: (...args) => log('[log]', '[nest-service]', ...args),
+  warn: (...args) => warn(color('\x1b[33m%s\x1b[0m'), '[warn]', '[nest-service]', ...args),
+  info: (...args) => info(color('\x1b[34m%s\x1b[0m'), '[info]', '[nest-service]', ...args),
+  error: (...args) => info(color('\x1b[31m%s\x1b[0m'), '[error]', '[nest-service]', ...args),
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(APP_CONFIG.PORT);
 }
 bootstrap();
